ba0b985322cfc60d352d74c8fab3c49da4456593
panchaliSam <panchalisam20030117@gmail.com>
2025-03-10 10:02:22 +0530

    Complete order preference

--- Smart-Convenience-Shop/src/main/java/preference_consumer/ServiceActivator.java
+++ Smart-Convenience-Shop/src/main/java/preference_consumer/ServiceActivator.java
@@ -11,18 +11,14 @@ public class ServiceActivator implements BundleActivator {
     public void start(BundleContext context) throws Exception {
         System.out.println("...STARTING PREFERENCE CONSUMER...");
 
-        // Wait for the Preference Producer to be registered
         while (true) {
-            // Fetch the CustomerPreferenceService from the OSGi service registry
             CustomerPreferenceService customerPreferenceService =
                     (CustomerPreferenceService) context.getService(
                             context.getServiceReference(CustomerPreferenceService.class));
 
             if (customerPreferenceService != null) {
-                // Successfully retrieved the CustomerPreferenceService
                 System.out.println("✅ Personalized Recommendation Service REGISTERED.");
 
-                // Initialize the Scanner to read user input
                 Scanner scanner = new Scanner(System.in);
                 System.out.println("Please enter your email: ");
                 String email = scanner.nextLine();
@@ -30,14 +26,13 @@ public class ServiceActivator implements BundleActivator {
                 System.out.println("Please enter your preference: ");
                 String preference = scanner.nextLine();
 
-                // Set the customer preference
                 customerPreferenceService.setPreference(email, preference);
 
                 System.out.println("Customer preference for " + email + " has been set to: " + preference);
-                break;  // Exit the loop once done
+                break;
             } else {
                 System.out.println("Waiting for Customer Preference Service...");
-                Thread.sleep(1000);  // Wait for the service to be available
+                Thread.sleep(1000);
             }
         }
     }
